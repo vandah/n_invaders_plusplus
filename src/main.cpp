60c5162d25f1ad0299a2409e25c97634fd5903bd
Vanda Hendrychová <hendrvan@fit.cvut.cz>
2017-05-07 00:01:14 +0200

    time control

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,37 @@
-/** 
+/**
  * @mainpage
  * @section Description
- * 
+ *
  * delsi popis
- * 
+ *
  * Created by Vanda Hendrychova
  */
 #include "main.hpp"
+
+view gui;
+
 int main(int argc,char ** argv){
-	get_opts(argc, argv);	// evaluate command line parameters
+	get_opts(argc, argv);
 	start_timer();
-	view gui;
 	while(1)gui.read_input();
+	finish();
 	return 0;
 }
 
+void start_timer(){
+	struct itimerval timer;
+
+	timer.it_value.tv_sec = 0;
+	timer.it_value.tv_usec = 1000000 / FPS;
+	timer.it_interval.tv_sec = 0;
+	timer.it_interval.tv_usec = 1000000 / FPS;
+
+	setitimer(ITIMER_REAL, &timer, NULL);
+
+	struct sigaction action;
+
+	action.sa_handler = &handle_timer; /* signal handler function */
+	action.sa_flags = SA_RESTART;
+
+	sigaction(SIGALRM, &action, NULL);
+}
