424625695106366f85b1f47892b33905532ac7cb
Vanda Hendrychová <hendrvan@fit.cvut.cz>
2017-05-06 21:44:46 +0200

    init

---
+++ src/object.hpp
@@ -0,0 +1,27 @@
+#include "globals.hpp"
+class object{
+	public:
+		object();
+		virtual void redraw()=0;
+		virtual void destroy()=0;
+	protected:
+		int size_x, size_y;
+		int pos_x, pos_y;
+};
+
+class moving:public object{
+	public:
+		void move(std::pair<int,int> xy);
+	protected:
+		int state;
+};
+
+class falling:public moving{};
+class missile:public falling{};
+class bonus:public falling{
+	public:
+		virtual void grant();
+};
+class smart:public moving{};
+class invader:public moving{};
+class player:public moving{};
