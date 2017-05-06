f6515cb1af43cb6baf5c7ae8bae9a7d5ad80e9a3
Vanda Hendrychová <hendrvan@fit.cvut.cz>
2017-05-07 00:41:37 +0200

    documentation comments

--- src/globals.hpp
+++ src/globals.hpp
@@ -1,10 +1,18 @@
+/**
+ * \file globals.hpp
+ * global includes and functions
+ */
+
 #include <unistd.h>
 #include <csignal>
 #include <string>
 #include <ncurses.h>
 #ifndef GLOBALS
 #define GLOBALS
+
+///version info
 extern void show_version();
+
 /// evaluate command line parameters
 extern void get_opts(int argc, char** argv);
 #endif
