aecc9d74440595bac9b2d6533c394060fdfbcf88
Vanda Hendrychová <hendrvan@fit.cvut.cz>
2017-05-07 00:01:07 +0200

    time control

--- src/main.hpp
+++ src/main.hpp
@@ -1,6 +1,14 @@
 #include "globals.hpp"
 #include "view.hpp"
+#include <sys/time.h>
 #ifndef MAIN
 #define MAIN
+
+#define FPS 50
+
 int main(int argc,char **argv);
+void start_timer();
+void handle_timer(int sig);
+void finish();
+
 #endif
