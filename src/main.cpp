5c6bf1fd8ba91dd05696fef8f1421f867cc4bb32
Vanda Hendrychová <hendrvan@fit.cvut.cz>
2017-05-06 23:05:46 +0200

    main function

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,10 @@
  */
 #include "main.hpp"
 int main(int argc,char ** argv){
-	cout<<"Ahoj, tady semestralka"<<endl;
+	get_opts(argc, argv);	// evaluate command line parameters
+	start_timer();
+	view gui;
+	while(1)gui.read_input();
 	return 0;
 }
+
--- src/main.hpp
+++ src/main.hpp
@@ -1,4 +1,6 @@
-#include <bits/stdc++.h>
-using namespace std;
-
+#include "globals.hpp"
+#include "view.hpp"
+#ifndef MAIN
+#define MAIN
 int main(int argc,char **argv);
+#endif
