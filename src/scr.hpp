e790cc4de228eabc31e4e04f88c5c5bed21cb576
Vanda Hendrychová <hendrvan@fit.cvut.cz>
2017-05-07 14:40:51 +0200

    scr_list fixes

--- src/scr.hpp
+++ src/scr.hpp
@@ -1,25 +1,28 @@
-#include "globals.hpp"
 #ifndef SCR
 #define SCR
+
+#include "globals.hpp"
+#include <vector>
+
 class scr{
 	public:
 		///variable inicialization
 		scr();
-		
+
 		///screen state handling
 		void set_state(int s);
 
 		///screen state handling
 		int get_state()const;
 
-		///screen main window handling
+		///main window handling
 		void set_win(WINDOW*w);
 
-		///screen main window handling
+		///main window handling
 		WINDOW* get_win()const;
-		
+
 		///destructor
-		virtual ~scr()=0;
+		virtual ~scr();
 
 		///updates the window
 		virtual void redraw()const=0;
@@ -38,23 +41,51 @@ class scr{
 		WINDOW *win;
 };
 
+///generic screen for listing items
 class scr_list:public scr{
 	public:
-		scr_list(const std::string* &list_items);
+		scr_list(const std::vector<std::string> &list_items);
+
+		///destructor
+		~scr_list();
+
+		///print list items
 		void draw_list();
+
+		///time tick
 		void handle_timer();
+		
+		///move selected idx up
+		void up();
+
+		///move selected idx down
+		void down();
+
+		///updates the window
+		virtual void redraw()const=0;
+		
+		///keypress event handler
+		virtual void handle_event(char event)=0;
 	protected:
-		std::string *items;
+		///menu items to print
+		std::vector<std::string> items;
+
+		///selected line
+		int selected_idx;
 };
 
+///main menu screen
 class scr_menu:public scr_list{
 	public:
 		scr_menu();
 		~scr_menu();
 		void redraw()const;
 		void handle_event(char event);
+	protected:
+		std::vector<int> screen_items;
 };
 
+///game settings
 class scr_settings:public scr_list{
 	public:
 		scr_settings();
@@ -63,6 +94,7 @@ class scr_settings:public scr_list{
 		void handle_event(char event);
 };
 
+///show high scores
 class scr_hiscore:public scr_list{
 	public:
 		scr_hiscore();
@@ -71,6 +103,7 @@ class scr_hiscore:public scr_list{
 		void handle_event(char event);
 };
 
+///game screen
 class scr_game:public scr{
 	public:
 		scr_game();
@@ -80,6 +113,7 @@ class scr_game:public scr{
 		void handle_timer();
 };
 
+///bye bye and quit
 class scr_quit:public scr{
 	public:
 		scr_quit();
