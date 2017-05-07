a7192ffa0fac6bdf97f16c9bf84a2062f9efb2d3
Vanda Hendrychová <hendrvan@fit.cvut.cz>
2017-05-07 09:59:20 +0200

    position handling

--- src/object.cpp
+++ src/object.cpp
@@ -1,1 +1,12 @@
 #include "object.hpp"
+object::object(int sx,int sy,int px,int py):size({sx,sy}),pos({px,py}){}
+
+void object::set_pos(std::pair<int,int> xy){pos=xy;}
+
+std::pair<int,int> object::get_pos(){return pos;}
+
+void moving::move(std::pair<int,int> xy){
+	//TODO check if not outside of the window
+	std::pair<int,int> curr_pos = get_pos();
+	set_pos({curr_pos.first+xy.first,curr_pos.second+xy.second});
+}
