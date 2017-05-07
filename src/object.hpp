a4e47d91afddda9ac5618b24470f2da7773eddc3
Vanda Hendrychová <hendrvan@fit.cvut.cz>
2017-05-07 09:58:56 +0200

    function declarations

--- src/object.hpp
+++ src/object.hpp
@@ -1,30 +1,71 @@
 #include "globals.hpp"
+///generic static object
 class object{
 	public:
-		object();
+		object(int sx=0,int sy=0,int px=0, int py=0);
 		virtual ~object();
 		virtual void redraw()=0;
 		virtual void destroy()=0;
-		virtual void reset()=0;
+		void set_pos(std::pair<int,int> xy);
+		std::pair<int,int> get_pos();
 	protected:
-		int size_x, size_y;
-		int pos_x, pos_y;
+		std::pair<int,int> size;
+		std::pair<int,int> pos;
 };
 
+///moving object
 class moving:public object{
 	public:
 		void move(std::pair<int,int> xy);
+		virtual void reset()=0;
 	protected:
 		int state;
 };
 
-class falling:public moving{};
-class missile:public falling{};
+class falling:public moving{
+	public:
+		void fall(bool down=true);
+		void redraw();
+		void destroy();
+		void reset();
+};
+
+class missile:public falling{
+	public:
+		void redraw();
+		void destroy();
+};
+
 class bonus:public falling{
 	public:
 		virtual void grant();
+		void redraw();
+		void destroy();
 };
-class smart:public moving{};
-class invader:public moving{};
-class ufo:public moving{};
-class player:public moving{};
+
+class smart:public moving{
+	public:
+		void redraw();
+		void destroy();
+};
+
+class invader:public moving{
+	public:
+		void redraw();
+		void destroy();
+		void reset();
+};
+
+class ufo:public moving{
+	public:
+		void redraw();
+		void destroy();
+		void reset();
+};
+
+class player:public moving{
+	public:
+		void redraw();
+		void destroy();
+		void reset();
+};
