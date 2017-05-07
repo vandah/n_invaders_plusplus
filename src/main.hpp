7854a079dc50d4723e061b8ff84a7211895cac7e
Vanda Hendrychová <hendrvan@fit.cvut.cz>
2017-05-07 09:52:52 +0200

    documentation comments+moved global var from cpp

--- src/main.hpp
+++ src/main.hpp
@@ -4,11 +4,19 @@
 #ifndef MAIN
 #define MAIN
 
+extern view gui;
+
 #define FPS 50
 
 int main(int argc,char **argv);
+
+///start timer and set time handler function
 void start_timer();
+
+///inform the gui about time event
 void handle_timer(int sig);
+
+///cleanup
 void finish();
 
 #endif
