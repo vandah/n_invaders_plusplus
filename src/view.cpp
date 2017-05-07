422975b479b1f20b2bc34492d236a458b51c45d4
Vanda Hendrychová <hendrvan@fit.cvut.cz>
2017-05-07 14:43:15 +0200

    simple implementation necessary for compilation

--- src/view.cpp
+++ src/view.cpp
@@ -6,8 +6,12 @@ view::view(){
 	keypad(scr_main,TRUE);
 	rows=LINES;
 	cols=COLS;
+	refresh();
+	current_screen=new scr_menu;
 	//TODO
 }
+
+view::~view(){}
 
 void view::switch_screen(int s){
 	scr *tmp_screen = current_screen;
@@ -15,17 +19,26 @@ void view::switch_screen(int s){
 		case SCR_MENU:
 			current_screen=new scr_menu;
 			break;
-		case SCR_GAME:
-			current_screen=new scr_game;
-			break;
-		case SCR_HISCORE:
-			current_screen=new scr_hiscore;
-			break;
-		case SCR_QUIT:
-			current_screen=new scr_quit;
-			break;
+//		case SCR_GAME:
+//			current_screen=new scr_game;
+//			break;
+//		case SCR_HISCORE:
+//			current_screen=new scr_hiscore;
+//			break;
+//		case SCR_SETTINGS:
+//			current_screen=new scr_settings;
+//			break;
+//		case SCR_QUIT:
+//			current_screen=new scr_quit;
+//			break;
 		default: /*NO DEFINED SCREEN*/
 			return;
 	}
 	delete tmp_screen;
 }
+
+void view::read_input(){} //TODO
+
+//void view::refresh(){} //T0DO
+
+void view::handle_timer(){current_screen->handle_timer();}
