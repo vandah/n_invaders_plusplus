7bf06a91582804f95f69a11b4cb526ae2efdbb84
Vanda Hendrychová <hendrvan@fit.cvut.cz>
2017-05-07 14:42:29 +0200

    scr_list and scr_menu fixes

--- src/scr.cpp
+++ src/scr.cpp
@@ -1,5 +1,9 @@
 #include "scr.hpp"
+#include "main.hpp"
+
 scr::scr():state(0),win(NULL){}
+
+scr::~scr(){}
 
 void scr::set_state(int s){state=s;}
 
@@ -9,4 +13,37 @@ void scr::set_win(WINDOW*w){win=w;}
 
 WINDOW* scr::get_win()const{return win;}
 
-scr_list::scr_list(const std::string* &list_items){}
+scr_list::scr_list(const std::vector<std::string> &list_items):selected_idx(0){
+	for(auto item:list_items)items.push_back(item); //copy list
+}
+
+scr_list::~scr_list(){}
+
+void scr_list::draw_list(){} //TODO
+
+void scr_list::handle_timer(){}//TODO
+
+void scr_list::up(){
+	selected_idx--;
+	selected_idx%=items.size();
+}
+
+void scr_list::down(){
+	selected_idx++;
+	selected_idx%=items.size();
+}
+
+scr_menu::scr_menu():scr_list(std::vector<std::string>{"Play","High Scores",
+		"Settings","Quit"}){}
+
+scr_menu::~scr_menu(){}
+
+void scr_menu::redraw()const{}
+
+void scr_menu::handle_event(char event){
+	switch(event){
+		case 10:
+			gui.switch_screen(SCR_GAME);
+			break;
+	}
+}
