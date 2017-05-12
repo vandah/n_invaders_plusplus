a17dc444db10dc086f57507d4e86b7f6f381f339
Vanda Hendrychová <hendrvan@fit.cvut.cz>
2017-05-12 19:15:14 +0200

    removed useless functions

--- src/object.cpp
+++ src/object.cpp
@@ -3,12 +3,7 @@ object::object(int sx,int sy,int px,int py):size({sx,sy}),pos({px,py}){}
 
 object::~object(){}
 
-void object::set_pos(std::pair<int,int> xy){pos=xy;}
-
-std::pair<int,int> object::get_pos(){return pos;}
-
 void moving::move(std::pair<int,int> xy){
 	//TODO check if not outside of the window
-	std::pair<int,int> curr_pos = get_pos();
-	set_pos({curr_pos.first+xy.first,curr_pos.second+xy.second});
+	pos={pos.first+xy.first,pos.second+xy.second};
 }
