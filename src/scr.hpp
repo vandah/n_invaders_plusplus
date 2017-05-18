a5242fbed25e9caf4ee2d7563b10a5f251fa3501
Vanda Hendrychová <hendrvan@fit.cvut.cz>
2017-05-19 00:33:02 +0200

    removed scr game and moved it to another file

--- src/scr.hpp
+++ src/scr.hpp
@@ -29,14 +29,4 @@ class scr{
 		WINDOW *win;
 };
 
-///game screen
-class scr_game:public scr{
-	public:
-		scr_game();
-		~scr_game();
-		void redraw()const;
-		void handle_event(int event);
-		void handle_timer();
-};
-
 #endif
