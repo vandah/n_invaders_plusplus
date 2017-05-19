46ddb1381c4d375f96d9e90fd41f3cf8a3f081fa
Vanda Hendrychová <hendrvan@fit.cvut.cz>
2017-05-19 12:28:48 +0200

    added some command line options

--- src/globals.cpp
+++ src/globals.cpp
@@ -1,12 +1,22 @@
 #include "globals.h"
+#include "main.h"
 
 void show_version(){
-	std::cout<<"Version number 0.0.1-1"<<std::endl;
-} //TODO
+	std::cout<<"Version number "<<VERSION<<std::endl;
+	finish(0);
+}
+
+void show_help(){
+	std::cout<<"HELP"<<std::endl;
+	//TODO write help
+	finish(0);
+}
 
 void get_opts(int argc, char** argv){
 	for(int i=0;i<argc;++i){
 		std::string opt=argv[i];
 		if(opt=="--version")show_version();
+		else if(opt=="--help"||opt=="-h")show_help();
+		//TODO other options
 	}
-} //TODO
+}
