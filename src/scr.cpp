afb72fdefcdab677f127c1f5e5e131efc309a68d
Vanda Hendrychová <hendrvan@fit.cvut.cz>
2017-05-20 17:55:34 +0200

    new variables for middle column and current row

--- src/scr.cpp
+++ src/scr.cpp
@@ -1,6 +1,6 @@
 #include "scr.h"
 #include "main.h"
 
-scr::scr():state(0),win(NULL){}
+scr::scr():state(0),win(NULL),midcol(0),current_row(0){}
 
 scr::~scr(){}
--- src/scr.h
+++ src/scr.h
@@ -4,28 +4,32 @@
 #include "globals.h"
 
 class scr{
-	public:
-		///variable inicialization
-		scr();
+    public:
+        ///variable inicialization
+        scr();
 
-		///destructor
-		virtual ~scr();
+        ///destructor
+        virtual ~scr();
 
-		///updates the window
-		virtual void redraw()const=0;
+        ///updates the window
+        virtual void redraw()const=0;
 
-		///keypress event handler
-		virtual void handle_event(int event)=0;
+        ///keypress event handler
+        virtual void handle_event(int event)=0;
 
-		///time tick
-		virtual void handle_timer()=0;
+        ///time tick
+        virtual void handle_timer()=0;
 
-	protected:
-		///screens are moving between several states
-		int state;
+    protected:
+        ///screens are moving between several states
+        int state;
 
-		///the main window
-		WINDOW *win;
+        ///the main window
+        WINDOW *win;
+
+        int midcol;
+
+        int current_row;
 };
 
 #endif
