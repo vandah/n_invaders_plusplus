99b919ae04b449577e3ea29cb70c4d5284a2f735
Vanda Hendrychová <hendrvan@fit.cvut.cz>
2017-05-27 18:34:44 +0200

    renamed files

--- src/scr/scr_hiscore.cpp
+++
@@ -1,8 +0,0 @@
-#include "scr_hiscore.h"
-scr_hiscore::scr_hiscore()
-    : scr_list()
-{
-  screen_title = "HIGH SCORES";
-}
-
-scr_hiscore::~scr_hiscore() {}
--- src/scr/scr_quit.cpp
+++
@@ -1,6 +0,0 @@
-#include "scr_quit.h"
-scr_quit::scr_quit() {}
-scr_quit::~scr_quit() {}
-void scr_quit::redraw() const {}
-void scr_quit::handle_event(int event) {}
-void scr_quit::handle_timer() {}
