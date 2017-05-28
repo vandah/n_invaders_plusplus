99a3f56cba0279029c34683eb582baa97dceb760
Vanda Hendrychová <hendrvan@fit.cvut.cz>
2017-05-28 19:47:21 +0200

    make the simulation more real with a number that I can achieve with basic arithmetic operations

--- invaders.sh
+++ invaders.sh
@@ -53,11 +53,11 @@ while :;do
 			"\033[1;35m$in12\n\033[1;32m$in22\n\033[1;34m$in32\n\033[1;36m$in42\n\033[1;33m$in52\033[0m"\
 			"\033[1;35m$in13\n\033[1;32m$in23\n\033[1;34m$in33\n\033[1;36m$in43\n\033[1;33m$in53\033[0m"\
 			"\033[1;35m$in14\n\033[1;32m$in24\n\033[1;34m$in34\n\033[1;36m$in44\n\033[1;33m$in54\033[0m"
-	do echo -e $i; sleep 0.15; clear; done;done
+	do echo -e $i; sleep 0.16; clear; done;done
 	for i in\
 		"\033[1;31m$ds11\n$ds21\n$ds31\n$ds41\n$ds51\033[0m"\
 		"\033[1;31m$ds12\n$ds22\n$ds32\n$ds42\n$ds52\033[0m"\
 		"\033[1;31m$ds13\n$ds23\n$ds33\n$ds43\n$ds53\033[0m"
-do echo -e $i; sleep 0.15; clear; done
+do echo -e $i; sleep 0.16; clear; done
 sleep 0.7
 done
