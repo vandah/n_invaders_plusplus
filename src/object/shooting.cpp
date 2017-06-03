2c93d15e35e8f0375e5d4d4dc86ae4d69f0396f2
Vanda Hendrychová <hendrvan@fit.cvut.cz>
2017-06-03 21:09:43 +0200

    position+destroy

--- src/object/shooting.cpp
+++ src/object/shooting.cpp
@@ -4,8 +4,16 @@ void shooting::reset() {}
 
 void shooting::missile_base::set_pos(std::pair<int, int> new_pos)
 {
+  old_pos = pos;
   pos = new_pos;
-  old_pos = { pos.first, pos.second };
 }
 
 shooting::missile_base::~missile_base() {}
+
+void shooting::missile_base::destroy()
+{
+  next_look();
+  redraw();
+
+  object::destroy();
+}
